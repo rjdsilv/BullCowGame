43600e28486dc26c8971d3fea5e234bddf63e35a
Rodrigo Januario da Silva <rjdsilv@gmail.com>
2018-03-25 19:11:11 -0400

    Finished another video.

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -3,23 +3,35 @@
 
 using namespace std;
 
+void PrintIntro();
+string GetGuess();
+
 int main()
 {
-	constexpr int WORLD_LENGTH = 5;
+	PrintIntro();
+	
+	// Repeat the guess back to the player.
+	cout << "Your guess was: " << GetGuess() << endl;
+	cout << endl;
 
+	return 0;
+}
+
+void PrintIntro()
+{
 	// Introduce the game.
+	constexpr int WORLD_LENGTH = 5;
 	cout << "Welcome to Bulls and Cows, a fun word game." << endl;
 	cout << "Can you guess the " << WORLD_LENGTH << " letter isogram I'm thinking of?" << endl;
 	cout << endl;
+	return;
+}
 
+string GetGuess()
+{
 	// Get a guess from the player.
 	string Guess = "";
 	cout << "Please, enter your guess: ";
 	getline(cin, Guess);
-
-	// Repeat the guess back to the player.
-	cout << "Your guess was: " << Guess << endl;
-	cout << endl;
-
-	return 0;
+	return Guess;
 }
