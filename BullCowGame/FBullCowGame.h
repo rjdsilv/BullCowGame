465f21e55fd386fdcd00d7644b1876d933e1f8f3
Rodrigo Januario da Silva <rjdsilv@gmail.com>
2018-03-30 17:03:43 -0400

    Finished Section_02_33

--- BullCowGame/FBullCowGame.h
+++ BullCowGame/FBullCowGame.h
@@ -5,10 +5,11 @@
 class FBullCowGame
 {
 public:
+	int GetMaxTries() const;
+	int GetCurrentTry() const;
+	bool IsGameWon() const;
+
 	void Reset(); // TODO make a more rich return value.
-	int GetMaxTries();
-	int GetCurrentTry();
-	bool IsGameWon();
 	bool CheckGuessValidity(std::string);
 
 private:
